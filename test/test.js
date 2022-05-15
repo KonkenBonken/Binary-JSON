15c4b8b9e53e542b78a088bcc69ca5678bc9015c
KonkenBonken <konrad05pettersson@gmail.com>
2022-05-16 00:11:10 +0200

    Added writeFile()

--- test/test.js
+++ test/test.js
@@ -5,8 +5,8 @@ import fs from 'fs';
 const source = JSON.parse(fs.readFileSync('sample.json', 'utf-8').trim());
 console.log(source);
 
-const parsed = parse(source);
-fs.writeFileSync('sample.kjo', parsed);
+const parsed = parse(source)
+	.writeFile('sample.kjo', true);
 
 const serialized = serialize(parsed);
 console.log(serialized);
